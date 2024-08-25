ad90a0e383ff3e21f9b2be6132a22f5539270bc0
tomcatmew <helltomcat@gmail.com>
2024-08-25 19:20:22 +0900

    asd

--- content/mouse-review.md
+++ content/mouse-review.md
@@ -6,3 +6,33 @@ Category: Review
 mew
 mewmewmewmew
 mewmewmewmew
+
+## Comments
+
+{% for comment in article.slug | get_comments %}
+<div class="comment">
+    <p><strong>{{ comment.author }}</strong> said on {{ comment.date }}:</p>
+    <p>{{ comment.body }}</p>
+</div>
+{% endfor %}
+
+<form action="https://github.com/tomcatmew/blog_main/issues" method="POST">
+    <input type="hidden" name="title" value="Comment on {{ title }}">
+    <input type="hidden" name="labels" value="comment">
+    
+    <p>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="body[name]" required>
+    </p>
+    <p>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="body[email]" required>
+    </p>
+    <p>
+        <label for="comment">Comment:</label>
+        <textarea id="comment" name="body[comment]" required></textarea>
+    </p>
+    <p>
+        <button type="submit">Submit Comment</button>
+    </p>
+</form>
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,6 @@
 AUTHOR = 'tomcat'
 SITENAME = 'personal blog'
-SITEURL = ""
+SITEURL = 'https://tomcatmew.github.io/blog_main'
 
 PATH = "content"
 
@@ -8,12 +8,22 @@ TIMEZONE = 'Japan'
 
 DEFAULT_LANG = 'English'
 
+ARTICLE_URL = '{slug}.html'
+ARTICLE_SAVE_AS = '{slug}.html'
+PAGE_URL = 'pages/{slug}.html'
+PAGE_SAVE_AS = 'pages/{slug}.html'
+CATEGORY_URL = 'category/{slug}.html'
+CATEGORY_SAVE_AS = 'category/{slug}.html'
+TAG_URL = 'tag/{slug}.html'
+TAG_SAVE_AS = 'tag/{slug}.html'
+
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+# FEED_ALL_ATOM = None
+# CATEGORY_FEED_ATOM = None
+# TRANSLATION_FEED_ATOM = None
+# AUTHOR_FEED_ATOM = None
+# AUTHOR_FEED_RSS = None
+
 
 # Blogroll
 LINKS = (
@@ -33,3 +43,22 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+
+import yaml
+from pathlib import Path
+
+def get_comments(article_slug):
+    comments_path = Path(f'content/data/comments/')
+    comments = []
+
+    if comments_path.exists():
+        for comment_file in comments_path.glob(f'{article_slug}-*.md'):
+            with comment_file.open('r') as f:
+                comment = yaml.safe_load(f)
+                comments.append(comment)
+
+    return comments
+
+JINJA_FILTERS = {
+    'get_comments': get_comments,
+}
